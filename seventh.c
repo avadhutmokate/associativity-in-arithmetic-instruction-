674aecf2c1a79b7897097a1af6d1c5f40cd55b7a
Avadhut Mokate <mokateavadhut@gmail.com>
2025-02-16 23:04:59 +0530

    Add files via upload

---
+++ seventh.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+// associativity (for saome precendence) left to right 
+int main() {
+    int a = 4 * 3 / 6 * 2;
+    printf("%d \n" , a);
+    return 0;
+
+
+
+}
